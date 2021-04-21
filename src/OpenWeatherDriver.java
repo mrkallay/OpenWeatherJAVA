7a9e609febfd2270517e3082a3835d8d213bed2e
Brian Kallay <brian.kallay@gmail.com>
2021-04-21 11:20:02 -0700

    minor updates

--- src/OpenWeatherDriver.java
+++ src/OpenWeatherDriver.java
@@ -1,14 +1,23 @@
 /**
  * Created by briankallay on 3/4/20.
  */
+import java.util.Scanner;
 public class OpenWeatherDriver
 {
     public static void main(String[] args) throws Exception
     {
-        OpenWeatherRequest owr = new OpenWeatherRequest();
-        //owr.setQuery("q=london,uk");
-        owr.setQuery("zip=94705");
-        owr.getWeatherData();
-        System.out.println("Current Temperature for 94705: " + owr.getTemperature());
+        Scanner in = new Scanner(System.in);
+
+        while(true)
+        {
+            System.out.println("What zip code would you like the weather for?");
+            String zip = in.nextLine();
+
+            OpenWeatherRequest owr = new OpenWeatherRequest();
+            //owr.setQuery("q=london,uk");
+            owr.setQuery("zip=" + zip + "&units=imperial");
+            owr.getWeatherData();
+            System.out.println("Current Temperature for " + zip + ": " + owr.getTemperature());
+        }
     }
 }
